a26b21b459fd2f35c00d61fb664a4932d0189306
Cecilia Brito Santos <ceciliabritosantos@gmail.com>
2022-10-07 15:16:06 -0300

    instalando mongoose

--- src/server.js
+++ src/server.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
+app.get("/", (request, response) => {
+    return response.render("index.ejs");
+}
+)
 app.listen(3000);
